ef3fab13919bc5489012145552bed1af59da7b65
Shadoki <uzinatorcl@gmail.com>
2019-11-17 17:01:52 -0800

    added a unit test to make sure the book class is working properly refactored code to create a statistics class instead of console.writelining things

--- src/gradebook/Book.cs
+++ src/gradebook/Book.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System;
 
 namespace gradebook {
-  class Book
+  public class Book
   {
     public Book(string name) {
       this.grades = new List<double>();
@@ -11,22 +11,18 @@ namespace gradebook {
     public void AddGrade(double grade) {
         grades.Add(grade);
     }
-    public void GetStatistics() {
-      double result = 0.0;
-      var highestValue = double.MinValue;
-      var lowestValue = double.MaxValue;
+    public Statistics GetStatistics() {
+      var result = new Statistics();
+      result.High = double.MinValue;
+      result.Low = double.MaxValue;
       foreach (double grade in grades) {
-        result += grade;
-        highestValue = Math.Max(grade, highestValue);
-        lowestValue = Math.Min(grade, lowestValue);
+        result.Average += grade;
+        result.High = Math.Max(grade, result.High);
+        result.Low = Math.Min(grade, result.Low);
       }
-      result /= grades.Count;
-      Console.WriteLine($"Here are the results for {name}");
-      Console.WriteLine($"The total amount of grades you entered were {grades.Count}");
-      Console.WriteLine($"The average was {result}");
-      Console.WriteLine($"The highest grade was {highestValue}");
-      Console.WriteLine($"The lowest grade was {lowestValue}");
+      result.Average /= grades.Count;
 
+      return result;
     }
     List<double> grades;
     string name;
---
+++ test/Gradebook.tests/BookTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace gradebook.tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var book = new Book("");
+            book.AddGrade(95.5);
+            book.AddGrade(65.7);
+            book.AddGrade(90.8);
+            var stats = book.GetStatistics();
+
+            Assert.Equal(stats.High, 95.5, 1);
+            Assert.Equal(stats.Low, 65.7, 1);
+            Assert.Equal(stats.Average, 84, 1);
+        }
+    }
+}
